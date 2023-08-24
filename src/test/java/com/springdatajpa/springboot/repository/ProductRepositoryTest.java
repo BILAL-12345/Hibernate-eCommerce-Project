f4b98276fc501bbcdb44c589bd1f6a9559915749
BILAL-12345 <bilalbhutto121@gmail.com>
2023-08-24 18:01:12 +0500

    done query methods.

--- src/test/java/com/springdatajpa/springboot/repository/ProductRepositoryTest.java
+++ src/test/java/com/springdatajpa/springboot/repository/ProductRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,12 +21,12 @@ class ProductRepositoryTest {
     void saveMethod(){
         // create product
         Product product = new Product();
-        product.setName("product 2");
-        product.setDescription("product 2 description");
-        product.setSku("120ABC");
-        product.setPrice(new BigDecimal(200));
+        product.setName("product");
+        product.setDescription("product description");
+        product.setSku("100ABC");
+        product.setPrice(new BigDecimal(100));
         product.setActive(true);
-        product.setImageUrl("product2.png");
+        product.setImageUrl("product.png");
 
         // save product
         Product savedObject = productRepository.save(product);
@@ -48,5 +49,86 @@ class ProductRepositoryTest {
 
         // save update entity
         productRepository.save(product);
+    }
+
+    @Test
+    void findByIdMethod(){
+
+        Long id = 1L;
+        Product product = productRepository.findById(id).get();
+    }
+
+    @Test
+    void saveAllMethod(){
+
+        // create product
+        Product product = new Product();
+        product.setName("product 3");
+        product.setDescription("product 3 description");
+        product.setSku("130ABC");
+        product.setPrice(new BigDecimal(300));
+        product.setActive(true);
+        product.setImageUrl("product3.png");
+
+        // create product
+        Product product4 = new Product();
+        product4.setName("product 4");
+        product4.setDescription("product 4 description");
+        product4.setSku("140ABC");
+        product4.setPrice(new BigDecimal(400));
+        product4.setActive(true);
+        product4.setImageUrl("product4.png");
+
+        productRepository.saveAll(List.of(product, product4));
+    }
+
+    @Test
+    void findAllMethod(){
+        List<Product> products = productRepository.findAll();
+
+        products.forEach((p)->{
+            System.out.println(p.getName());
+        });
+    }
+
+    @Test
+    void deleteByIdMethod(){
+        Long id = 1L;
+        productRepository.deleteById(id);
+    }
+
+    @Test
+    void deleteMethod(){
+
+        // find an entity by id
+        Long id = 2L;
+        Product product = productRepository.findById(id).get();
+
+        // delete(entity)
+        productRepository.delete(product);
+    }
+
+    @Test
+    void deleteAllMethod(){
+
+        //productRepository.deleteAll();
+
+        Product product = productRepository.findById(5L).get();
+        Product product2 = productRepository.findById(6L).get();
+
+        productRepository.deleteAll(List.of(product,product2));
+    }
+
+    @Test
+    void countMethod(){
+        long count = productRepository.count();
+        System.out.println(count);
+    }
+
+    @Test
+    void existsByIdMethod(){
+        Long id = 7L;
+        boolean result = productRepository.existsById(id);
+        System.out.println(result);
     }
 }
